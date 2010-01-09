165b9cd7772f6e3623da8508403ef9bc14e5ce55
bongotastic <bongotastic@gmail.com>
2010-01-09 14:22:03 +0000

    minor mods

--- lib/sandbox_position.py
+++ lib/sandbox_position.py
@@ -11,55 +11,39 @@ class position_descriptor(vect_5D):
     '''! \ brief A class which treats the position of an entity as a polygon instead of a point.
     Now that the position is no longer a point, some assumption made in earlier version of the code may not work, keep an eye for this.
     Regardless, point manips can still be done on the positition descriptor as they get applied to he centroid of the polygon instead.
-    
+
     '''
     def __init__(self, X=0.0, Y=0.0, footprint = None, translator=None):
         # The coordinate translator
         self.MGRS = translator
-        
+
         # the footprint
         self.footprint = footprint
         if type(self.footprint) == type([]):
             self.footprint = sandbox_geometry.base_polygon(self.footprint)
-            
+
         if hasattr(X,'x'):
             # case where a vector is provided as first argument
             vect_5D.__init__(self, X.x, X.y)
         else:
             # case where the data is passed as intended
             vect_5D.__init__(self,X,Y)
-    
-    '''    
-    def __getattr__(self, name):
-        ''! \brief Attempts to pass on the call to the footprint.
-        ''
-        try:
-            ''if hasattr(self, name):
-                return getattr(self, name)''
-            if hasattr(self.footprint, name):
-                return getattr(self.footprint, name)
-        except:
-            pass
-      
-        # All else fail  
-        raise AttributeError, name
-    '''
-    
+
     # Information
     def AsVect(self):
         '''! \brief Similar as type casting to a vect_5D
         '''
         return vect_5D(self.x, self.y, self.z, self.course, self.rate)
-    
+
     def Radius(self):
         '''!
            Mean distance for all vertices to centroid.
         '''
         return self.footprint.Radius()
-    
+
     def PointInside(self, P):
         return self.footprint.PointInside(P)
-    
+
     def Overlaps(self, poly):
         '''! \brief call the method on the foot print.
         '''
@@ -78,6 +62,8 @@ class position_descriptor(vect_5D):
             self.z = ct.z
             return True
         return False
+    
+    
     def Set(self, pos):
         '''
            Set position and footprint centered around centroid.
@@ -85,7 +71,7 @@ class position_descriptor(vect_5D):
         '''
         # Compute offset
         offset = pos - self
-        
+
         # offset
         self.x = pos.x
         self.y = pos.y
@@ -93,12 +79,12 @@ class position_descriptor(vect_5D):
         if pos.__class__ == vect_5D().__class__:
             self.course = pos.course
             self.rate = pos.rate
-            
+
         # Set Footprint
         self.footprint.Translate(offset)        
-        
-    
-    
+
+
+
     def SetFootprint(self, fp):
         # Make sure that the footprint is a geometrical object, then attach and set 
         # position to the new footprint's centroid
@@ -106,7 +92,7 @@ class position_descriptor(vect_5D):
             ref = fp.Centroid()
         except:
             return False
-        
+
         # Set the fp's centroid to current position
         # pos = ref + diff
         # diff = pos - ref
@@ -114,13 +100,13 @@ class position_descriptor(vect_5D):
         #fp.Translate(diff)
         self.footprint = fp
         self.ToCentroid()
-        
+
     def Step(self):
         vect_5D.Step(self)
         # Move all the footprint control point
         trans = vect_5D(course=self.course,move=self.rate).Project()
         self.footprint.Translate(trans)
-        
+
     def toXML(self, doc, node, coord_translator):
         '''Unusual interface that doesn't create a node, but add to an existing one.'''
         # Case of location defined as a coordinate
@@ -130,23 +116,32 @@ class position_descriptor(vect_5D):
         nd.appendChild(doc.doc.createTextNode(mycoord))
         # Add location to position descriptor
         doc.AddNode(nd, node)
-        
+
         # Altitude TODO
-        
+
         # FOOTPRINT TODO
-    
+
     def fromXML(self, doc, node):
-      # Not in use because it needs to be handled at the sim level (to solve for coordinates).
-      # Special case where the node is a (naked) location node.
+        # Not in use because it needs to be handled at the sim level (to solve for coordinates).
+        # Location node
+        loc = doc.Get(node, 'location')
+        if loc != '':
+            self.fromXMLlocation(doc, loc)
+            
+    def fromXMLlocation(self, doc, node):
+        ''' Load in the location node only (useful as it is an option in scenario deifinition.
+        '''
+        # Full PD node
+        nl = doc.Get(node, 'named_location')
+        if nl:
+            # TODO, yet to be supported feature
+            pass
         
-      # Full PD node
-      nl = doc.Get(node, 'named_location')
-      if nl:
-        # TODO, yet to be supported feature
-        pass
-      # Coordinates
-      cd = doc.Get(node, 'coordinates')
-      if cd and self.sim and self.sim.map:
-        # Create a position descriptor from scratch
-        self['position'] = position_descriptor(self.sim.map.MGRS.AsVect(cd))
-      # Footprint TODO ticket#17
+        # Coordinates
+        cd = doc.Get(node, 'coordinates')
+        if cd and self.sim and self.sim.map:
+            # Create a position descriptor from scratch
+            vec = self.translator.AsVect(cd)
+            self.x = vec.x
+            self.y = vec.y
+        
